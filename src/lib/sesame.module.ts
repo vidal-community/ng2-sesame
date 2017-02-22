3527b3e41836e5bb39170c36901e891b30cbacc4
Julien Cheype <cheype@gmail.com>
2017-02-22 12:02:20 +0100

    export jwtUtils

--- src/lib/sesame.module.ts
+++ src/lib/sesame.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { HttpModule, Http } from '@angular/http';
-import {SesameService, JwtUtils,SESAME_CONFIG} from './sesame.service';
+import {SesameService, JwtUtils, SESAME_CONFIG} from './sesame.service';
 
 @NgModule({
     imports: [HttpModule]
