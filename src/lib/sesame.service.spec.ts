36e114a4e47b77055f150cfdbc1090c226fd99b8
Stéphane Petit <spetit@enjoycode.fr>
2022-11-29 11:49:07 +0100

    Extract HTTP calls in a dedicated service
    This will help us to reduce coupling and make the sesame service more readable.
    
    Also, it will help us a lot for testing in order to spy and mock HTTP calls.
    
    So we refactored tests with Jasmine Spys

--- src/lib/sesame.service.spec.ts
+++ src/lib/sesame.service.spec.ts
@@ -1,7 +1,7 @@
-import {fakeAsync, flush, TestBed, tick} from '@angular/core/testing';
-import {JwtUtils, SESAME_CONFIG, SesameService, UserInfo} from './sesame.service';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {TestBed} from '@angular/core/testing';
+import {JWT_COOKIE, JwtUtils, SesameService, UserInfo} from './sesame.service';
+import {SesameHttpService} from './sesame-http.service';
+import {of} from 'rxjs';
 
 let sesame: SesameService;
 
@@ -11,12 +11,26 @@ const userInfoStub = {
   roles: ['ROLE1', 'ROLE2']
 } as UserInfo;
 
+const sesameHttpMock = {
+  'getPem' : jasmine.createSpy('getPem'),
+  'getJwtToken' : jasmine.createSpy('getJwtToken'),
+  'faceUrl' : jasmine.createSpy('faceUrl'),
+  'check' : jasmine.createSpy('check'),
+  'logout' : jasmine.createSpy('logout')
+};
+
 const jwtToken = 'dummy.' + btoa(JSON.stringify(userInfoStub)) + '.dummy';
 const cookie = 'authentication-jwt=dummy.eyJ1c2VybmFtZSI6InRvdG8iLCJtYWlsIjoidG90b0B2aWRhbC5mciIsImxhbmd1YWdlIjp7ImNvZGUiOiJmcmEiLCJsYWJlbCI6IkZyYW5jYWlzIn0sInJvbGVzIjpbIlJPTEUxIiwiUk9MRTIiXX0=.dummy';
 
+function clearMocks() {
+  sesameHttpMock.getPem.calls.reset();
+  sesameHttpMock.getJwtToken.calls.reset();
+  sesameHttpMock.faceUrl.calls.reset();
+  sesameHttpMock.check.calls.reset();
+  sesameHttpMock.logout.calls.reset();
+}
+
 describe('SesameService', () => {
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
   const jwtUtils = new JwtUtils();
 
   jwtUtils.validate = () => {
@@ -24,22 +38,22 @@ describe('SesameService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule, HttpClientTestingModule],
       providers: [
-        {provide: SESAME_CONFIG, useValue: {apiEndpoint: 'http://sesame/api'}},
+        {provide: SesameHttpService, useValue: sesameHttpMock},
         {provide: JwtUtils, useValue: jwtUtils},
         SesameService
       ]
     });
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
-    sesame = TestBed.inject(SesameService);
+    clearMocks();
+    sesameHttpMock.getPem.and.returnValue(of('pem'));
   });
 
   describe('when already logged with cookie', () => {
 
     beforeEach(() => {
       document.cookie = cookie;
+      sesameHttpMock.check.and.returnValue(of(jwtToken));
+      sesame = TestBed.inject(SesameService);
     });
 
     it('should retrieve UserInfo', () => {
@@ -50,6 +64,7 @@ describe('SesameService', () => {
     });
 
     it('should logout', () => {
+      sesameHttpMock.logout.and.returnValue(of(null));
       sesame.logout();
       sesame.userInfo().subscribe((userInfo) => {
           expect(userInfo).toBe(undefined);
@@ -70,37 +85,34 @@ describe('SesameService', () => {
     });
 
     it('should check server and retrieve logged user with cookie', () => {
-      const keysReq = httpTestingController.expectOne('http://sesame/api/keys/public');
-      const checkReq = httpTestingController.expectOne('http://sesame/api/user/jwt/check');
-
-      keysReq.flush('FAKE_KEY');
-      checkReq.flush(jwtToken);
-      httpTestingController.verify();
+      expect(sesameHttpMock.getPem).toHaveBeenCalledTimes(1);
+      expect(sesameHttpMock.getJwtToken).toHaveBeenCalledTimes(0);
+      expect(sesameHttpMock.check).toHaveBeenCalledTimes(1);
     });
 
   });
 
   describe('when not logged', () => {
+    beforeEach(() => {
+      document.cookie = JWT_COOKIE + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';;
+      sesame = TestBed.inject(SesameService);
+    });
 
     it('should not check server when cookie is not present', () => {
-      httpTestingController.expectNone('http://sesame/api/keys/public');
-      httpTestingController.expectNone('http://sesame/api/user/jwt/check');
-
-      httpTestingController.verify();
+      expect(sesameHttpMock.getPem).toHaveBeenCalledTimes(1);
+      expect(sesameHttpMock.getJwtToken).toHaveBeenCalledTimes(0);
+      expect(sesameHttpMock.check).toHaveBeenCalledTimes(0);
     });
 
     it('should call server for login', () => {
+
+      sesameHttpMock.getJwtToken.and.returnValue(of(jwtToken));
+
       sesame.login('toto', 'password');
 
-      const keysReqs = httpTestingController.expectOne('http://sesame/api/keys/public');
-      keysReqs.flush('FAKE_KEY');
-
-      const req = httpTestingController.expectOne('http://sesame/api/user/jwt');
-      expect(req.request.method).toEqual('GET');
-      expect(req.request.withCredentials).toBeTruthy();
-      expect(req.request.headers.get('Authorization')).toBe('Basic ' + btoa('toto:password'));
-
-      httpTestingController.verify();
+      expect(sesameHttpMock.getPem).toHaveBeenCalledTimes(1);
+      expect(sesameHttpMock.getJwtToken).toHaveBeenCalledTimes(1);
+      expect(sesameHttpMock.check).toHaveBeenCalledTimes(0);
     });
   });
 
--- src/lib/sesame.service.ts
+++ src/lib/sesame.service.ts
@@ -1,15 +1,10 @@
-import { Injectable, Inject, InjectionToken } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, of, ReplaySubject, zip} from 'rxjs';
-export const SESAME_CONFIG = new InjectionToken('sesame.config');
 import * as jsrsasign from 'jsrsasign';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {catchError, map, mergeMap} from 'rxjs/operators';
+import {map, mergeMap} from 'rxjs/operators';
+import {SesameHttpService} from './sesame-http.service';
 
-export interface SesameConfig {
-  apiEndpoint: string;
-}
-
-const JWT_COOKIE = 'authentication-jwt';
+export const JWT_COOKIE = 'authentication-jwt';
 
 export interface UserInfo {
   jwt: string;
@@ -17,6 +12,7 @@ export interface UserInfo {
   username: string;
   mail: string;
 }
+
 export type UserInfoCallback = (userInfo: UserInfo, userInfoOld?: UserInfo) => void;
 
 @Injectable({
@@ -32,7 +28,7 @@ export class JwtUtils {
 
   parse(jwt) {
     const jwtSplit = jwt.split('.');
-    return  JSON.parse(jsrsasign.b64utos(jwtSplit[1]));
+    return JSON.parse(jsrsasign.b64utos(jwtSplit[1]));
   }
 }
 
@@ -44,17 +40,16 @@ export class SesameService {
   private pemObservable: Observable<string>;
   private userInfoObservable = new ReplaySubject<UserInfo>(1);
 
-
-  constructor(private http: HttpClient,
-    @Inject(SESAME_CONFIG) private sesameConfig: any,
-    private jwtUtils: JwtUtils) {
-    this.pemObservable = http.get(`${sesameConfig.apiEndpoint}/keys/public`, {responseType: 'text'});
+  constructor(private sesameHttp: SesameHttpService,
+              private jwtUtils: JwtUtils) {
+    this.pemObservable = sesameHttp.getPem();
     this.check();
   }
 
   public hasAnyRoles(roles: Array<string>): Observable<boolean> {
-    return this.userInfo().pipe(map(userInfo =>
-      roles.some((role) => userInfo && userInfo.roles && (userInfo.roles.indexOf(role) !== -1))
+    return this.userInfo().pipe(map(userInfo => {
+        return roles.some((role) => userInfo && userInfo.roles && (userInfo.roles.indexOf(role) !== -1));
+      }
     ));
   };
 
@@ -64,16 +59,8 @@ export class SesameService {
 
   public login(username, password): void {
     this.doOnUserInfo(undefined);
-    const authdata = btoa(username + ':' + password);
-    const headers = new HttpHeaders({
-      'Authorization': `Basic ${authdata}`
-    });
 
-    const jwtObservable = this.http
-      .get(`${this.sesameConfig.apiEndpoint}/user/jwt`, {
-        withCredentials: true, headers,
-        responseType: 'text'
-      });
+    const jwtObservable = this.sesameHttp.getJwtToken(username, password);
 
     zip(jwtObservable, this.pemObservable)
       .subscribe(([jwt, pem]) =>
@@ -82,7 +69,7 @@ export class SesameService {
   }
 
   public logout(): void {
-    this.http.get(`${this.sesameConfig.apiEndpoint}/user/jwt/logout`, { withCredentials: true }).subscribe(() => {
+    this.sesameHttp.logout().subscribe(() => {
       this.deleteCookie(JWT_COOKIE);
       this.doOnUserInfo(undefined);
     });
@@ -102,9 +89,7 @@ export class SesameService {
   }
 
   public faceUrl(login: string): Observable<string> {
-    return this.http
-      .get(`${this.sesameConfig.apiEndpoint}/face/${login}`, {responseType: 'text'})
-      .pipe(catchError(() => of(undefined)));
+    return this.sesameHttp.faceUrl(login);
   }
 
   private checkJwt(jwt, pem): UserInfo {
@@ -112,7 +97,6 @@ export class SesameService {
     const userInfo = this.jwtUtils.parse(jwt);
     this.setCookie(JWT_COOKIE, jwt, 360, '/');
     userInfo.jwt = jwt;
-    this.doOnUserInfo(userInfo);
     return userInfo;
   };
 
@@ -150,11 +134,7 @@ export class SesameService {
     if (!this.getCookie(JWT_COOKIE)) {
       return;
     }
-    const httpCheck = this.http
-      .get(`${this.sesameConfig.apiEndpoint}/user/jwt/check`, {
-        withCredentials: true,
-        responseType: 'text'
-      });
+    const httpCheck = this.sesameHttp.check();
     zip(this.pemObservable, httpCheck)
       .subscribe(([pem, jwt]) => {
           this.checkJwt(jwt, pem);
